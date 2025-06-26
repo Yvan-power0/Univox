d6b423322c69c1bf7577e36502eb8591213a97b3
Yvan-power0 <koffimoayechristyvan@gmail.com>
2025-06-26 01:32:38 +0000

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,36 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const jwt    = require("jsonwebtoken");
+const User   = require("../models/User");
 
-// Récupérer tous les utilisateurs en ligne (simulation)
-router.get("/online", async (req, res) => {
+function auth(req, res, next) {
+  const h = req.header("Authorization");
+  if (!h) return res.status(401).send("Token manquant");
   try {
-    const users = await User.find().select("username avatar _id friends");
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    req.userId = jwt.verify(h.split(" ")[1], process.env.JWT_SECRET).id;
+    next();
+  } catch {
+    res.status(401).send("Token invalide");
   }
+}
+
+// Online (tous vérifiés)
+router.get("/online", auth, async (req, res) => {
+  const list = await User.find({ verified: true }).select("username avatar _id");
+  res.json(list);
+});
+
+// Liste d'amis
+router.get("/friends", auth, async (req, res) => {
+  const u = await User.findById(req.userId).populate("friends","username avatar _id");
+  res.json(u.friends);
+});
+
+// Ajouter ami
+router.post("/friends/add", auth, async (req, res) => {
+  const { targetId } = req.body;
+  await User.findByIdAndUpdate(req.userId,   { $addToSet: { friends: targetId } });
+  await User.findByIdAndUpdate(targetId,     { $addToSet: { friends: req.userId } });
+  res.json({ message: "Demande envoyée / Ami ajouté." });
 });
 
 module.exports = router;
