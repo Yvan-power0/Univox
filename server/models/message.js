145c835cd02bea28b199fd72d7a1068cda71fec2
Yvan-power0 <koffimoayechristyvan@gmail.com>
2025-06-26 01:29:41 +0000

    Update message.js

--- server/models/message.js
+++ server/models/message.js
@@ -2,26 +2,12 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema(
   {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    seen: {
-      type: Boolean,
-      default: false,
-    }
+    sender:   { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
+    content:  { type: String, required: true },
+    seen:     { type: Boolean, default: false }
   },
-  { timestamps: true } // ⏱️ createdAt et updatedAt auto
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Message", MessageSchema);
