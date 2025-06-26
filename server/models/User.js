a0a87d33610650d38e894086d81f18ff017037f9
Yvan-power0 <koffimoayechristyvan@gmail.com>
2025-06-26 01:25:32 +0000

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,33 +1,13 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    minlength: 3
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  avatar: {
-    type: String,
-    default: ""
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  username:  { type: String, required: true, minlength: 3 },
+  email:     { type: String, required: true, unique: true },
+  password:  { type: String, required: true, minlength: 6 },
+  avatar:    { type: String, default: "" },
+  verified:  { type: Boolean, default: false },
+  friends:   [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("User", UserSchema);
